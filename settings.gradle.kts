4e31c954dda1118c3c13f1c0f19e811470c04d7d
L00162395 <71789175+L00162395@users.noreply.github.com>
2022-04-17 22:10:08 +0100

    Update settings.gradle.kts
    Adding automatic build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,21 @@
  * This project uses @Incubating APIs which are subject to change.
  */
 
+
+                      
 rootProject.name = "Gradle Demo"
 
 println("In the settings.gradle.kts file - This is executable during initialisation phase")
 println("In the settings,gradle.kts file - Determine which project take part in the build")
 include("app")
+
+plugins {
+  id("com.gradle.enterprise") version("3.9)
+  }
+  
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
